2b2741dfbe07cba3c19c80113924aec25e0c8ed6
Arabongo <edosurfer118@gmail.com>
2023-03-22 16:27:36 +0100

    Esercizio 1 oop

---
+++ _1/Course.java
@@ -0,0 +1,15 @@
+package javaoop._1;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class Course {
+    String courseName;
+    Integer maxStudent;
+    Integer qualityRatingOutOf10;
+    String professorName;
+    String nameOfSchool;
+    List<Course> students = new ArrayList<Course>();
+
+}
