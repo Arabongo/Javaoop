52e4dc64c0c98bf259bbe3253bac2d5d54adf098
Arabongo <edosurfer118@gmail.com>
2023-04-11 16:48:51 +0200

    modificato esercizio

--- _2/Student.java
+++ _2/Student.java
@@ -4,45 +4,44 @@ public class Student {
     private String name;
     private int age;
 
-   public Student(String name, int age) {
+    public Student(String name, int age) {
         this.name = name;
         this.age = age;
     }
-    public Student(String name){
-        this(name, 0);
+
+    public Student(String name) {
+        this.name = name;
     }
-    public Student(int age){
-       this("",age);
+
+    public Student(int age) {
+        this.age = age;
     }
+
     public Student() {
-       this("",0);
-   }
-    public String getName(){
-       return name;
-    }
-    public void setName(String name){
-       this.name = name;
-    }
-    public int getAge(){
-       return age;
-    }
-    public void setAge(int age){
-       this.age = age;
     }
 
-   public String toString() {
-        if (!name.isEmpty() && age != 0) {
-            return name + " " + age;
-        } else if (!name.isEmpty()) {
-            return name;
-        } else if (age != 0) {
-            return Integer.toString(age);
-        } else {
-            return "Studente senza nome ed età";
-        }
+    public String getName() {
 
+        return name;
+    }
 
+    public void setName(String name) {
 
+        this.name = name;
+    }
 
-}}
+    public int getAge() {
+
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String toString() {
+        return name + " " + age;
+
+    }
+}
 
